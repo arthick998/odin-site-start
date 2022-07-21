4a7404a39dce91fa36d442bbe81a1a3e1caa1fcd
arthick998 <rakshitbhagat9582@gmail.com>
2022-07-21 16:48:35 +0530

    variables

--- js/javascript.js
+++ js/javascript.js
@@ -3,3 +3,6 @@ let message;
 message = "Hello"; //    'Hello message from server';
 
 alert(message); //alert the message;
+console.log(23 + 97); //alert the message of additional
+console.log((4 + 6 + 9) / 77); //Add a sequence of 6 different numbers together.
+document.getElementById("footer").innerHTML = message;
