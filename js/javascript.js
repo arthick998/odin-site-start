c7f958f3c753ad8c535193e93ed32c334eed589d
arthick998 <rakshitbhagat9582@gmail.com>
2022-07-21 17:12:29 +0530

    number task

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,13 @@
 let message;
 let x = (4 + 6 + 9) / 77;
+let a = 10;
+let b = a * 7;
+
+console.log(a);
+console.log(a * 9);
+console.log(b);
 
 message = "Hello"; //    'Hello message from server';
-//alert the message;
+console.log(message);
 console.log(23 + 97); //alert the message of additional
 console.log(x.toFixed(5)); //
