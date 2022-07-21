08b725b9f91cfe33dd4a0c9a67447a9333a1cb3c
arthick998 <rakshitbhagat9582@gmail.com>
2022-07-21 17:00:30 +0530

    number introduced

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,7 @@
 let message;
+let x = (4 + 6 + 9) / 77;
 
 message = "Hello"; //    'Hello message from server';
-
-alert(message); //alert the message;
+//alert the message;
 console.log(23 + 97); //alert the message of additional
-console.log((4 + 6 + 9) / 77); //Add a sequence of 6 different numbers together.
-document.getElementById("footer").innerHTML = message;
+console.log(x.toFixed(5)); //
