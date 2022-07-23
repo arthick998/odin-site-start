0c44704ec91c1220ef7b167d31cb1823ba2012cc
arthick998 <rakshitbhagat9582@gmail.com>
2022-07-23 16:50:44 +0530

    string intro

--- js/javascript.js
+++ js/javascript.js
@@ -4,6 +4,7 @@ let a = 10;
 let b = a * 7;
 const max = 57;
 let actual = max - 13;
+const string = "the revolution will not be televised.";
 
 console.log(actual);
 
@@ -15,3 +16,4 @@ message = "Hello"; //    'Hello message from server';
 console.log(message);
 console.log(23 + 97); //alert the message of additional
 console.log(x.toFixed(5)); //
+console.log(string); // string is been introduced
