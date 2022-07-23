7280ac14291101fd11fb5b8b93aeca263eef7dc6
arthick998 <rakshitbhagat9582@gmail.com>
2022-07-23 17:18:15 +0530

    qoutes string

--- js/javascript.js
+++ js/javascript.js
@@ -4,8 +4,15 @@ let a = 10;
 let b = a * 7;
 const max = 57;
 let actual = max - 13;
-const string = 'the revolution will not be televised.';
+const string = "the revolution will not be televised.";
 const badstring = string;
+const sgl = "Single quotes.";
+const dbl = "Double quotes";
+const bigmouth = "I've got no right to take my place…";
+console.log(bigmouth);
+
+console.log(sgl);
+console.log(dbl);
 
 console.log(actual);
 
